c24b35569b6ba90bfd0559e58fc491b121301d19
Kristjan T <kristjan.taniel@gmail.com>
2024-10-06 15:15:23 +0300

    - Redesigned WAILA panel - Added a type flair to WAILA - Slight changes to what information is shown for Characters - Added basic WAILA info for trees and plants

--- Scripts/WAILA.lua
+++ Scripts/WAILA.lua
@@ -21,6 +21,7 @@ WAILA.inspectable = {
     BEARING = 7,
     HARVESTABLE = 8,
     CONSUMABLE = 9,
+    TERRAIN_ASSET = 10,
     UNKNOWN = 404,
     [1] = "INTERACTABLE",
     [2] = "SHAPE",
@@ -31,6 +32,7 @@ WAILA.inspectable = {
     [7] = "BEARING",
     [8] = "HARVESTABLE",
     [9] = "CONSUMABLE",
+    [10] = "TERRAIN_ASSET",
     [404] = "UNKNOWN"
 }
 
@@ -51,16 +53,86 @@ WAILA.interactableType = {
 WAILA.vanillaCharacters = {
     [sm.uuid.new("264a563a-e304-430f-a462-9963c77624e9")] = "Woc",
 
-    ["48c03f69-3ec8-454c-8d1a-fa09083363b1"] = "Glowbug",
-    ["04761b4a-a83e-4736-b565-120bc776edb2"] = "Tapebot (variant 1)",
-    ["9dbbd2fb-7726-4e8f-8eb4-0dab228a561d"] = "Tapebot (variant 2)",
-    ["fcb2e8ce-ca94-45e4-a54b-b5acc156170b"] = "Tapebot (variant 3)",
-    ["68d3b2f3-ed4b-4967-9d22-8ee6f555df63"] = "Tapebot (variant 4)",
-    ["c3d31c47-0c9b-4b07-9bd4-8f022dc4333e"] = "Explosive Tapebot",
-    ["8984bdbf-521e-4eed-b3c4-2b5e287eb879"] = "Totebot",
-    ["c8bfb8f3-7efc-49ac-875a-eb85ac0614db"] = "Haybot",
-    ["9f4fde94-312f-4417-b13b-84029c5d6b52"] = "Farmbot",
-    ["b6cafd3e-970b-4974-bb9f-ba7184b02797"] = "Builderbot"
+    ["48c03f69-3ec8-454c-8d1a-fa09083363b1"] = { name = "Glowbug", icon = "icon_glowbug.png" },
+    ["04761b4a-a83e-4736-b565-120bc776edb2"] = { name = "Tapebot (variant 1)", icon = "icon_tapebot_blue.png" },
+    ["9dbbd2fb-7726-4e8f-8eb4-0dab228a561d"] = { name = "Tapebot (variant 2)", icon = "icon_tapebot_blue.png" },
+    ["fcb2e8ce-ca94-45e4-a54b-b5acc156170b"] = { name = "Tapebot (variant 3)", icon = "icon_tapebot_blue.png" },
+    ["68d3b2f3-ed4b-4967-9d22-8ee6f555df63"] = { name = "Tapebot (variant 4)", icon = "icon_tapebot_blue.png" },
+    ["c3d31c47-0c9b-4b07-9bd4-8f022dc4333e"] = { name = "Explosive Tapebot", icon = "icon_tapebot_red.png" },
+    ["8984bdbf-521e-4eed-b3c4-2b5e287eb879"] = { name = "Totebot", icon = "icon_totebot_green.png" },
+    ["c8bfb8f3-7efc-49ac-875a-eb85ac0614db"] = { name = "Haybot", icon = "icon_haybot.png" },
+    ["9f4fde94-312f-4417-b13b-84029c5d6b52"] = { name = "Farmbot", icon = "icon_farmbot.png" },
+    ["b6cafd3e-970b-4974-bb9f-ba7184b02797"] = { name = "Builderbot", icon = "../unknown_object.png" }
+}
+
+-- Max title length: 51 char
+
+
+WAILA.vanillaHarvestables = {
+    ["b39349ae-9b7e-48e2-8e9d-6f9dc6472fd6"] = { name = "Soil", iconUUID = "9a3e478c-2224-44fa-887c-239965bd05ad" },
+    ["bb600268-cd29-4715-babe-5fd02645eb1c"] = { name = "Blueberry Plant (growing)", iconUUID = "6a43fff2-8c6d-4460-9f44-e5483b5267dd" },
+    ["9b031ec3-bc91-47b9-93fb-df79a8ef3026"] = { name = "Blueberry Plant (ready for harvesting)", iconUUID = "6a43fff2-8c6d-4460-9f44-e5483b5267dd" },
+    ["d3fdedca-7e1c-45cc-a1db-f0deee381a71"] = { name = "Banana Plant (growing)", iconUUID = "aa4c9c5e-7fc6-4c27-967f-c550e551c872" },
+    ["80cd6e60-154f-46da-8f26-ff30f2961fa2"] = { name = "Banana Plant (ready for harvesting)", iconUUID = "aa4c9c5e-7fc6-4c27-967f-c550e551c872" },
+    ["18efedc5-8706-4ecb-afd4-e9294d3f1052"] = { name = "Redbeet Plant (growing)", iconUUID = "4ce00048-f735-4fab-b978-5f405e60f48f" },
+    ["1a167796-8162-4990-97a7-b3810b3f94ca"] = { name = "Redbeet Plant (ready for harvesting)", iconUUID = "4ce00048-f735-4fab-b978-5f405e60f48f" },
+    ["6dd177f4-3312-4b1e-a986-4421b5e83bff"] = { name = "Carrot Plant (growing)", iconUUID = "47ece75a-bfca-4e8a-b618-4f609fcea0da" },
+    ["18990ae9-62d1-41f7-aab9-5199b31e3e89"] = { name = "Carrot Plant (ready for harvesting)", iconUUID = "47ece75a-bfca-4e8a-b618-4f609fcea0da" },
+    ["c6f80a93-5b16-45ef-a478-ca56a50f61ae"] = { name = "Tomato Plant (growing)", iconUUID = "6d92d8e7-25e9-4698-b83d-a64dc97978c8" },
+    ["534b13a0-3ec1-4558-a77a-cfccf6c3cb3e"] = { name = "Tomato Plant (ready for harvesting)", iconUUID = "6d92d8e7-25e9-4698-b83d-a64dc97978c8" },
+    ["b1a17952-b6a2-436d-81e4-df8ffb552166"] = { name = "Orange Plant (growing)", iconUUID = "f5098301-1693-457b-8efc-83b3504105ac" },
+    ["133ae17a-c038-4a55-ab79-f40f6840ab2b"] = { name = "Orange Plant (ready for harvesting)", iconUUID = "f5098301-1693-457b-8efc-83b3504105ac" },
+    ["ec1cf82f-e8f3-4ca6-8e35-a4bdf0e8e259"] = { name = "Potato Plant (growing)", iconUUID = "bfcfac34-db0f-42d6-bd0c-74a7a5c95e82" },
+    ["c42ad18d-797b-4196-ab79-86e222d8f767"] = { name = "Potato Plant (ready for harvesting)", iconUUID = "bfcfac34-db0f-42d6-bd0c-74a7a5c95e82" },
+    ["1337f492-aa23-42d0-af7a-fae45b47e55f"] = { name = "Pineapple Plant (growing)", iconUUID = "4ec64cda-1a5b-4465-88b4-5ea452c4a556" },
+    ["2cedbc23-92c5-4fba-8a08-ecbae23a28e5"] = { name = "Pineapple Plant (ready for harvesting)", iconUUID = "4ec64cda-1a5b-4465-88b4-5ea452c4a556" },
+    ["1675314b-0dfc-4d34-b854-0bdf0476221d"] = { name = "Broccoli Plant (growing)", iconUUID = "b5cdd503-fe1c-482b-86ab-6a5d2cc4fc8f" },
+    ["b166b142-792a-4c8f-9d06-f277dab6dba6"] = { name = "Broccoli Plant (ready for harvesting)", iconUUID = "b5cdd503-fe1c-482b-86ab-6a5d2cc4fc8f" },
+    ["779b5e09-7ce7-4a16-9817-02f5cb8e11f6"] = { name = "Cotton Plant (growing)", iconUUID = "3440440b-d362-4473-aa03-b7c41e1fe7ad" },
+    ["4d923a91-0c98-40f9-ae3f-ef386362ab1c"] = { name = "Cotton Plant (ready for harvesting)", iconUUID = "3440440b-d362-4473-aa03-b7c41e1fe7ad" },
+    ["7261bb49-9c63-4097-999a-4dcab9645eb0"] = { name = "Beehive", iconUUID = "fcf0958c-084d-4854-9b1b-b06594b4262a" },
+    ["3f1507bc-fc48-4b77-b87a-687cb1f4b787"] = { name = "Beehive (harvested)", iconUUID = "fcf0958c-084d-4854-9b1b-b06594b4262a" },
+    ["c591d94b-d7d1-4305-a9dd-76ef06d6fb49"] = { name = "Wild Cotton Plant", iconUUID = "3440440b-d362-4473-aa03-b7c41e1fe7ad" },
+    ["062d34ac-d7ed-44ef-b523-e5b3ad70fdac"] = { name = "Wild Cotton Plant (growing)", iconUUID = "3440440b-d362-4473-aa03-b7c41e1fe7ad" },
+    ["f7567939-d170-437e-b5c4-352ee9d5850d"] = { name = "Pigment flower", iconUUID = "c9396a42-67c3-4fa3-b682-31428ff9eced" },
+    ["1c5bdfa2-e28f-4ccd-8a4e-be52d812f18a"] = { name = "Pigment flower (growing)", iconUUID = "c9396a42-67c3-4fa3-b682-31428ff9eced" },
+    ["2ab8edca-9cfe-4b1c-9f57-5092f94ea890"] = { name = "Oil Geyser", iconUUID = "1147e59d-6940-42b4-840b-07f05054f5e0" },
+    ["b6a26689-d803-49cc-a6e8-7f2b66dfb54d"] = { name = "Oil Geyser (harvested)", iconUUID = "1147e59d-6940-42b4-840b-07f05054f5e0" },
+    ["39a5aeba-a021-4117-8cad-e08ad159281d"] = { name = "Wild Corn", iconUUID = "fe8bfeba-850b-4827-9785-10e2468c9c23" },
+    ["0f74d3a4-ade0-4054-8207-21f6be7903be"] = { name = "Wild Corn (harvested)", iconUUID = "fe8bfeba-850b-4827-9785-10e2468c9c23" },
+    ["046a8ac8-300b-4e52-91f7-0912d53b2955"] = { name = "Slimy Clam", iconUUID = "40e8bd0d-04a0-4e95-b593-4038b54b156f" },
+    ["8c95cede-25a5-48b4-be97-6c16dab31057"] = { name = "Slimy Clam (harvested)", iconUUID = "40e8bd0d-04a0-4e95-b593-4038b54b156f" },
+    ["c4ea19d3-2469-4059-9f13-3ddb4f7e0b79"] = { name = "Birch Tree", iconUUID = "" },
+    ["711c3e72-7ba1-4424-ae70-c13d23afe818"] = { name = "Birch Tree", iconUUID = "" },
+    ["a7aa52af-4276-4b2d-af44-36bc41864e04"] = { name = "Birch Tree", iconUUID = "" },
+    ["91ec04ea-9bf7-4a9d-bb7f-3d0125ff78c7"] = { name = "Leafy Tree", iconUUID = "" },
+    ["4d482999-98b7-4023-a149-d47be709b8f7"] = { name = "Leafy Tree", iconUUID = "" },
+    ["3db0a60d-8668-4c8a-8dd2-f5ceb294977e"] = { name = "Leafy Tree", iconUUID = "" },
+    ["73f968f0-d3a3-4334-86a8-a90203a3a56d"] = { name = "Spruce Tree", iconUUID = "" },
+    ["86324c5b-e97a-41f6-aa2c-7c6462f1f2e7"] = { name = "Spruce Tree", iconUUID = "" },
+    ["27aa53ea-1e09-4251-a284-437f93850409"] = { name = "Spruce Tree", iconUUID = "" },
+    ["8411caba-63db-4b93-ad67-7ae8e350d360"] = { name = "Pine Tree", iconUUID = "" },
+    ["1cb503a4-9306-412f-9e13-371bc634af60"] = { name = "Pine Tree", iconUUID = "" },
+    ["fa864e51-67db-4ac9-823b-cfbdf523375d"] = { name = "Pine Tree", iconUUID = "" },
+    ["0d3362ae-4cb3-42ae-8a08-d3f9ed79e274"] = { name = "Small Stone", iconUUID = "" },
+    ["f6b8e9b8-5592-46b6-acf9-86123bf630a9"] = { name = "Small Stone", iconUUID = "" },
+    ["60ad4b7f-a7ef-4944-8a87-0844e6305513"] = { name = "Small Stone", iconUUID = "" },
+    ["ab5b947e-a223-4842-83dd-aa6b23ac2b86"] = { name = "Medium Stone", iconUUID = "" },
+    ["5da6c862-8a5c-4b56-90d3-5f038d569c4a"] = { name = "Medium Stone", iconUUID = "" },
+    ["90e0ef6a-8409-4459-8926-e5351d7da611"] = { name = "Medium Stone", iconUUID = "" },
+    ["ab362045-0444-4749-9f24-f5e850162857"] = { name = "Large Stone", iconUUID = "" },
+    ["63fb92b3-e1dc-4b5c-9ed3-7b572bc01ca4"] = { name = "Large Stone", iconUUID = "" },
+    ["67111401-1ee1-4bfb-8780-fa878352f90d"] = { name = "Large Stone", iconUUID = "" },
+    ["b8eb68ca-25a0-47aa-8745-12f2dd13d873"] = { name = "Loot Crate", iconUUID = "" },
+    ["a6b9fa26-df80-4f5b-9679-418f4567e5ca"] = { name = "Epic Loot Crate", iconUUID = "" },
+    ["97fe0cf2-0591-4e98-9beb-9186f4fd83c8"] = { name = "Dropped Item", iconUUID = "" },
+    ["74ffe0eb-17fe-41b3-a532-8dbb5924e5dc"] = { name = "loot_audiologfile", iconUUID = "" },
+    ["6e7611e1-ad18-413a-a4cb-bab7e3a5ca75"] = { name = "loot_picturelogfile", iconUUID = "" },
+    ["6726c35c-f8e6-49cf-b0b6-05e7efac4115"] = { name = "loot_informationlogfile", iconUUID = "" },
+    ["3b7054d0-9ca7-4efa-8b4a-e8658eda3181"] = { name = "Logbook", iconUUID = "" },
+    ["e3cb14d5-9d28-45e8-96f7-fc79c60de292"] = { name = "Rare Garment Box", iconUUID = "" },
+    ["cceec00b-8a34-4d68-a419-8c3bc7aa075a"] = { name = "Epic Garment Box", iconUUID = "" },
+    ["6757b211-f50c-42c5-bd7c-648dcbe3ed52"] = { name = "Glowstick Remains", iconUUID = "3a3280e4-03b6-4a4d-9e02-e348478213c9" }
 }
 
 WAILA.isShown = false
@@ -90,7 +162,7 @@ function WAILA.client_initializeGUI(self)
     self:client_setTitleLabel("Welcome to SM: WAILA")
     self:client_setPropertiesLabel(
         "You'll see information about the object you're looking here.\nFor some objects, special information is available.")
-    self:client_setPreview(sm.uuid.new("fdb8b8be-96e7-4de0-85c7-d2f42e4f33ce"))
+    self:client_setWAILAIcon(1)
     if (not self.gui:isActive()) then
         self.gui:open()
     end
@@ -109,12 +181,20 @@ function WAILA.client_displayPanel(self, raycastResult)
         return
     end
 
-    if (raycastResult:getShape() ~= nil) then
-        if (raycastResult:getShape().id == self.lastObjectId and raycastResult:getShape().uuid == self.lastObjectUUID) then
+    if (raycastResult:getShape()) then
+        local currentShapeId = raycastResult:getShape().id
+        local currentShapeUuid = raycastResult:getShape().uuid
+        if (currentShapeId == self.lastObjectId and currentShapeUuid == self.lastObjectUUID) then
             return
         end
-        self.lastObjectId = raycastResult:getShape().id
-        self.lastObjectUUID = raycastResult:getShape().uuid
+    end
+
+    if (raycastResult:getHarvestable()) then
+        local currentHarvestableId = raycastResult:getHarvestable().id
+        local currentHarvestableUuid = raycastResult:getHarvestable().uuid
+        if (currentHarvestableId == self.lastObjectId and currentHarvestableUuid == self.lastObjectUUID) then
+            return
+        end
     end
 
     if (self.gui:isActive()) then
@@ -150,7 +230,12 @@ function WAILA.client_displayPanel(self, raycastResult)
         end
     end
 
+    self:client_setTypeLabel(hitType)
+
     if (hitType == self.inspectable.SHAPE) then
+        self.lastObjectId = raycastResult:getShape().id
+        self.lastObjectUUID = raycastResult:getShape().uuid
+
         self:client_setColor(asShape.color)
         if (asShape.isBlock) then
             local _body = asBody
@@ -182,14 +267,18 @@ function WAILA.client_displayPanel(self, raycastResult)
             end
 
             self:client_setTitleLabel(sm.shape.getShapeTitle(asShape.uuid) .. " #FCC200x" .. blocks)
-
-            self:client_setPropertiesLabel("Mass: #FCC200" .. mass .. " kg")
+            self:client_setPropertiesLabel("Mass: #FCC200" ..
+                mass ..
+                " kg\n#FFFFFFBuoyancy: #FCC200" .. string.format("%.1f", asShape:getBuoyancy()))
             self:client_setPreview(asShape.uuid)
         else
             self:client_setTitleLabel(sm.shape.getShapeTitle(asShape.uuid))
             self:client_setPreview(asShape.uuid)
         end
     elseif (hitType == self.inspectable.INTERACTABLE) then
+        self.lastObjectId = raycastResult:getShape().id
+        self.lastObjectUUID = raycastResult:getShape().uuid
+
         local asInter = asShape:getInteractable()
         local type = self:client_getInteractableType(asInter)
         self:client_setPreview(asInter:getShape().uuid)
@@ -335,6 +424,9 @@ function WAILA.client_displayPanel(self, raycastResult)
             self:client_setColor(asInter.shape.color)
         end
     elseif (hitType == self.inspectable.CONSUMABLE) then
+        self.lastObjectId = raycastResult:getShape().id
+        self.lastObjectUUID = raycastResult:getShape().uuid
+
         local shape = raycastResult:getShape()
         self:client_setTitleLabel(sm.shape.getShapeTitle(shape.uuid))
         self:client_setPropertiesLabel(sm.shape.getShapeDescription(shape.uuid))
@@ -372,7 +464,8 @@ function WAILA.client_displayPanel(self, raycastResult)
             self:client_setTitleLabel(asChar:getPlayer().name)
         else
             if (table.hasKey(self.vanillaCharacters, tostring(asChar:getCharacterType()))) then
-                self:client_setTitleLabel(self.vanillaCharacters[tostring(asChar:getCharacterType())])
+                self:client_setTitleLabel(self.vanillaCharacters[tostring(asChar:getCharacterType())].name)
+                self:client_setCharacterIcon(self.vanillaCharacters[tostring(asChar:getCharacterType())].icon)
             else
                 self:client_setTitleLabel("Character ##" .. asChar:getId())
             end
@@ -386,15 +479,28 @@ function WAILA.client_displayPanel(self, raycastResult)
             end
             activeAnimations = string.gsub(activeAnimations, " ", "#ffffff, ")
 
-
             self:client_setPropertiesLabel(
-                "UUID: #FCC200" ..
-                tostring(asChar:getCharacterType()) ..
+                "Character id: #FCC200##" ..
+                tostring(asChar:getId()) ..
                 "\n#ffffffAnimation: " .. activeAnimations)
         end
-        self:client_setPreview(sm.uuid.new("068a89ca-504e-4782-9ede-48f710aeea73"))
-    elseif hitType == self.inspectable.UNKNOWN then
+        --self:client_setPreview(sm.uuid.new("068a89ca-504e-4782-9ede-48f710aeea73"))
+    elseif hitType == self.inspectable.HARVESTABLE then
+        self.lastObjectId = raycastResult:getHarvestable().id
+        self.lastObjectUUID = raycastResult:getHarvestable().uuid
 
+        local asHarvest = raycastResult:getHarvestable()
+
+        self:client_setTitleLabel(self.vanillaHarvestables[tostring(asHarvest.uuid)].name)
+        if (self.vanillaHarvestables[tostring(asHarvest.uuid)].iconUUID) then
+            self:client_setPreview(sm.uuid.new(self.vanillaHarvestables[tostring(asHarvest.uuid)].iconUUID))
+        end
+    elseif hitType == self.inspectable.TERRAIN_ASSET then
+        return
+    elseif hitType == self.inspectable.UNKNOWN then
+        self:client_setWAILAIcon(1)
+        self:client_setTitleLabel("Unknown part")
+        self:client_setPropertiesLabel("SMWAILA has no idea what that is.")
     end
     self.gui:open()
     self.isShown = true
@@ -413,7 +519,26 @@ end
 ---@param self WAILA
 ---@param shapeUUID Uuid The UUID for the shape whose icon to set the preview to
 function WAILA.client_setPreview(self, shapeUUID)
+    self.gui:setVisible("ObjectPreview", true)
+    self.gui:setVisible("ObjectPreviewAlternative", false)
+
     self.gui:setIconImage("ObjectPreview", shapeUUID)
+end
+
+function WAILA.client_setWAILAIcon(self, type)
+    self.gui:setVisible("ObjectPreview", false)
+    self.gui:setVisible("ObjectPreviewAlternative", true)
+    if (type == 1) then
+        self.gui:setImage("ObjectPreviewAlternative", "$CONTENT_DATA/Gui/Assets/unknown_object.png")
+    elseif (type == 2) then
+
+    end
+end
+
+function WAILA.client_setCharacterIcon(self, character_icon_file)
+    self.gui:setVisible("ObjectPreview", false)
+    self.gui:setVisible("ObjectPreviewAlternative", true)
+    self.gui:setImage("ObjectPreviewAlternative", "$CONTENT_DATA/Gui/Assets/Characters/" .. character_icon_file)
 end
 
 --- Sets the title label to <code>title</code>
@@ -430,14 +555,26 @@ function WAILA.client_setPropertiesLabel(self, properties)
     self.gui:setText("ObjectSubtitle", properties)
 end
 
+function WAILA.client_setTypeLabel(self, type)
+    local text = string.gsub(self.inspectable[type], "_", "")
+    self.gui:setText("TypeLabel", text)
+end
+
 --- Hides the SMWAILA panel
 --- @param self WAILA
 function WAILA.client_closePanel(self)
     self.gui:close()
     self:client_setTitleLabel("...")
     self:client_setPropertiesLabel("...")
-    self:client_setPreview(sm.uuid.new("fdb8b8be-96e7-4de0-85c7-d2f42e4f33ce"))
+    self:client_clearPreview()
+    self.lastObjectId = nil
+    self.lastObjectUUID = nil
     self.isShown = false
+end
+
+function WAILA.client_clearPreview(self)
+    self.gui:setVisible("ObjectPreviewAlternative", false)
+    self.gui:setVisible("ObjectPreview", false)
 end
 
 --- Returns a string representing the current mode of operation for the supplied <code>Interactable</code>
@@ -465,6 +602,9 @@ end
 --- @param raycastResult RaycastResult
 --- @return number type The type of object hit by the ray.
 function WAILA.client_getHitType(self, raycastResult)
+    if (raycastResult.type == "terrainAsset") then
+        return self.inspectable.TERRAIN_ASSET
+    end
     if (raycastResult:getShape() ~= nil) then
         if (raycastResult:getShape():getInteractable() ~= nil) then
             if (raycastResult:getShape():getInteractable().type == "itemStack") then
